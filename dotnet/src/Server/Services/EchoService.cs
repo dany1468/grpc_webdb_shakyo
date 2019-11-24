cd301e405f96b7d7fa7f66d9a821bbae6b4f3624
dany1468 <dany1468@gmail.com>
2019-11-25 07:50:58 +0900

    Add EchoService

---
+++ dotnet/src/Server/Services/EchoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Server
+{
+    public class EchoService : Echo.EchoService.EchoServiceBase
+    {
+        private readonly ILogger<EchoService> _logger;
+        public EchoService(ILogger<EchoService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<Echo.EchoResponse> Echo(Echo.EchoRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new Echo.EchoResponse
+            {
+                Message = request.Message
+            });
+        }
+    }
+}
--- dotnet/src/Server/Startup.cs
+++ dotnet/src/Server/Startup.cs
@@ -32,6 +32,7 @@ namespace Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<EchoService>();
 
                 endpoints.MapGet("/", async context =>
                 {
