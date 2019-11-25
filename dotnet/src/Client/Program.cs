8c2bb562bb5d53ee585b4b191503cb672e70327d
dany1468 <dany1468@gmail.com>
2019-11-25 09:23:49 +0900

    Implement Client

--- dotnet/src/Client/Program.cs
+++ dotnet/src/Client/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Echo.EchoService.EchoServiceClient(channel);
+            var reply = await client.EchoAsync(
+                              new Echo.EchoRequest { Message = "EchoClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
